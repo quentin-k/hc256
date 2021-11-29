c249f39817b793676db8b629e1e803933041e72c
quentin <49767738+quentin-k@users.noreply.github.com>
2021-11-29 09:27:03 -0600

    Updated encryption utility

--- hc256-util/src/main.rs
+++ hc256-util/src/main.rs
@@ -8,7 +8,7 @@ use hc256::Hc256;
 
 fn main() {
     let matches = App::new("Hc256 Encryption Utility")
-        .version("0.3.0")
+        .version("0.4.0")
         .author("Quentin K")
         .about("Encrypts a file using the hc-256 stream cipher")
         .arg(
@@ -44,6 +44,30 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
+            Arg::with_name("smart key")
+                .short("s")
+                .long("smartkey")
+                .value_name("SMART KEY")
+                .help("Attempts to base64 decode the key and if fails will try to open file, the order can be flipped with the <TOGGLE> flag")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("smart iv")
+                .short("S")
+                .long("smartiv")
+                .value_name("SMART IV")
+                .help("Attempts to base64 decode the initialization vector and if fails will try to open file, the order can be flipped with the <TOGGLE> flag")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("toggle")
+                .short("t")
+                .long("toggle")
+                .value_name("TOGGLE")
+                .help("Toggles the order of smart decoding resolution")
+                .takes_value(false)
+        )
+        .arg(
             Arg::with_name("file")
                 .short("f")
                 .long("file")
@@ -59,45 +83,122 @@ fn main() {
             .help("Specify an output file to use instead of inplace encryption"))
         .get_matches();
 
-    let key: [u8; 32] = {
-        let v = match matches.value_of("key") {
-            Some(v) => base64::decode(v).expect("You must supply a base64 encoded key"),
-            None => match matches.value_of("key file") {
-                Some(p) => {
-                    let mut v = Vec::new();
-                    let mut f = File::open(p).expect("You must enter a valid file path!");
-                    f.read_to_end(&mut v).expect("Failed to read file");
-                    v
-                }
+    let key: [u8; 32] = match (
+        matches.value_of("key"),
+        matches.value_of("key file"),
+        matches.value_of("smart key"),
+        matches.occurrences_of("toggle"),
+    ) {
+        (Some(v), _, _, _) => {
+            match get_base64_val(v) {
+                Some(v) => v,
                 None => {
                     eprintln!("You must supply either a base64 encoded key or key file");
                     exit(1);
                 }
-            },
-        };
-
-        v.try_into().expect("The key provided was not 32 bytes")
+            }
+        }
+        (_, Some(v), _, _) => {
+            match get_file_val(v) {
+                Some(v) => v,
+                None => {
+                    eprintln!("You must supply either a base64 encoded key or key file");
+                    exit(1);
+                }
+            }
+        }
+        (_, _, Some(v), t) => {
+            if t < 1 {
+                match get_base64_val(v) {
+                    Some(v) => v,
+                    None => {
+                        match get_file_val(v) {
+                            Some(v) => v,
+                            None => {
+                                eprintln!("You must supply either a base64 encoded key or key file");
+                                exit(1);
+                            }
+                        }
+                    }
+                }
+            } else {
+                match get_file_val(v) {
+                    Some(v) => v,
+                    None => {
+                        match get_base64_val(v) {
+                            Some(v) => v,
+                            None => {
+                                eprintln!("You must supply either a base64 encoded key or key file");
+                                exit(1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        _ => {
+            eprintln!("You must supply either a base64 encoded key or key file");
+            exit(1);
+        }
     };
 
-    let iv: [u8; 32] = {
-        let v = match matches.value_of("iv") {
-            Some(v) => base64::decode(v).expect("You must supply a base64 encoded iv"),
-            None => match matches.value_of("iv file") {
-                Some(p) => {
-                    let mut v = Vec::new();
-                    let mut f = File::open(p).expect("You must enter a valid file path!");
-                    f.read_to_end(&mut v).expect("Failed to read file");
-                    v
-                }
+    let iv: [u8; 32] = match (
+        matches.value_of("iv"),
+        matches.value_of("iv file"),
+        matches.value_of("smart iv"),
+        matches.occurrences_of("toggle"),
+    ) {
+        (Some(v), _, _, _) => {
+            match get_base64_val(v) {
+                Some(v) => v,
                 None => {
                     eprintln!("You must supply either a base64 encoded initialization vector or initialization vector file");
                     exit(1);
                 }
-            },
-        };
-
-        v.try_into()
-            .expect("The initialization vector provided was not 32 bytes")
+            }
+        }
+        (_, Some(v), _, _) => {
+            match get_file_val(v) {
+                Some(v) => v,
+                None => {
+                    eprintln!("You must supply either a base64 encoded initialization vector or initialization vector file");
+                    exit(1);
+                }
+            }
+        }
+        (_, _, Some(v), t) => {
+            if t < 1 {
+                match get_base64_val(v) {
+                    Some(v) => v,
+                    None => {
+                        match get_file_val(v) {
+                            Some(v) => v,
+                            None => {
+                                eprintln!("You must supply either a base64 encoded initialization vector or initialization vector file");
+                                exit(1);
+                            }
+                        }
+                    }
+                }
+            } else {
+                match get_file_val(v) {
+                    Some(v) => v,
+                    None => {
+                        match get_base64_val(v) {
+                            Some(v) => v,
+                            None => {
+                                eprintln!("You must supply either a base64 encoded initialization vector or initialization vector file");
+                                exit(1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        _ => {
+            eprintln!("You must supply either a base64 encoded initialization vector or initialization vector file");
+            exit(1);
+        }
     };
 
     let filename = matches.value_of("file").unwrap();
@@ -112,7 +213,41 @@ fn main() {
         Some(p) => p,
         None => filename,
     })
-        .unwrap()
-        .write_all(&content)
-        .expect("Failed to write content to file");
+    .unwrap()
+    .write_all(&content)
+    .expect("Failed to write content to file");
+}
+
+fn get_base64_val(val: &str) -> Option<[u8; 32]> {
+    let dv = base64::decode(val);
+
+    let dv = match dv {
+        Ok(v) => v,
+        Err(_) => return None,
+    };
+
+    if dv.len() == 32 {
+        Some(dv.try_into().unwrap())
+    } else {
+        None
+    }
+}
+
+fn get_file_val(val: &str) -> Option<[u8; 32]> {
+    let file = File::open(val);
+
+    let mut file = match file {
+        Ok(v) => v,
+        Err(_) => return None,
+    };
+
+    let mut fc = Vec::new();
+
+    if file.read_to_end(&mut fc).is_err() {
+        None
+    } else if fc.len() == 32 {
+        Some(fc.try_into().unwrap())
+    } else {
+        None
+    }
 }
